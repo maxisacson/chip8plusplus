e4e470680b8feaae68955e4ebe3c44905b011ae3
Max Isacson <max.isacson@physics.uu.se>
2016-05-26 17:25:46 +0200

    Wrote some sensible commentary

--- include/chip8.h
+++ include/chip8.h
@@ -1,10 +1,10 @@
 #ifndef CHIP8_H_
 #define CHIP8_H_
 
-#define MEMORY_SIZE 0x1000
-#define REGISTERS 16
-#define STACK_SIZE 16
-#define SCREEN_WIDTH 64
+#define MEMORY_SIZE   0x1000 // 4KB of memory
+#define REGISTERS     16
+#define STACK_SIZE    16
+#define SCREEN_WIDTH  64
 #define SCREEN_HEIGHT 32
 
 #include <cstdint>
@@ -21,22 +21,53 @@ class Chip8 {
         void dump_memory();
 
     private:
+        // Each opcode is 2 bytes (16 bits) long
         uint16_t m_opcode;
 
+        // The memory in the Chip8 consists of 4096 addresses each
+        // capable of storing 1 byte (8 bits)
         uint8_t m_memory[MEMORY_SIZE];
+
+        // The Chip8 has 16 1 byte (8 bits) registers
         uint8_t m_registers[REGISTERS];
-        
+       
+        // The address (index) register is capable of storing memory addresses. The address space is 
+        // 0x000-0xfff, so we need at least 12 bits for the encoding
         uint16_t m_address_register;
 
+        // The Chip8 is capable of storing 16 addresses in the stack, used when running subroutines.
+        // Since each entry in the stack is an address we need at least 12 bits to encode them
         uint16_t m_stack[STACK_SIZE];
-        
+       
+        // The stack pointer points to the top of the stack. It should ALWAYS point to a valid address if the
+        // stack is not empty, then it equals -1. To not fuck this up ALWAYS push the stack using
+        //      
+        //      m_stack[++m_stack_pointer] = some_address;
+        //
+        // and to pop from the stack ALWAYS use
+        //
+        //      some_address = m_stack[m_stack_pointer--];
+        //
+        // I.e. note the position of the increment/decrement operators. 
         int m_stack_pointer;
 
+        // The program counter always points to the memory address where the NEXT instruction is
+        // located. I.e., the program counter should ALWAYS be incremented by 2 (since each instruction
+        // is 2 bytes long) after an instruction has been read
+        // 
+        //      opcode = m_memory[m_program_counter];
+        //      m_program_counter += 2;
+        //
+        // to keep things internally consistent.
         uint16_t m_program_counter;
 
+        // The Chip8 has 2 timers, the delay timer and the sound timer, if they are > 0 they should be
+        // decremented by 1 at a rate of 60Hz, i.e. once every 1/60:th second.
         uint16_t m_delay_timer;
         uint16_t m_sound_timer;
 
+        // The screen of the Chip8 is 64 pixels wide and 32 pixels height. Each pixel has 2 states: ON (1)
+        // or OFF (0). When a pixel is on it is drawn, otherwise not.
         uint8_t m_screen_buffer[SCREEN_HEIGHT*SCREEN_WIDTH];
 };
 
--- src/chip8.cxx
+++ src/chip8.cxx
@@ -7,24 +7,30 @@ Chip8::Chip8() :
     m_opcode(0x0000),
     m_address_register(0x0000),
     m_stack_pointer(-1),
+    // We initialize the program counter to 0x200 (=512), since addresses 0x000-0x1ff (0-511) is not
+    // used (well, we store the font set there but no rom data should go before 0x200)
     m_program_counter(0x0200),
     m_delay_timer(0x0000),
     m_sound_timer(0x0000) {
 
     int i = 0;
 
+    // Clear the memory
     for (i = 0; i < MEMORY_SIZE; ++i) {
         m_memory[i] = 0x0;
     } 
 
+    // Clear all registers
     for (i = 0; i < REGISTERS; ++i) {
         m_registers[i] = 0x0;
     }
 
+    // Clear the stack
     for (i = 0; i < STACK_SIZE; ++i) {
         m_stack[i] = 0x0;
     }
 
+    // Clear the screen
     for (i = 0; i < SCREEN_WIDTH*SCREEN_HEIGHT; ++i) {
         m_screen_buffer[i] = 0x0;
     }
@@ -34,6 +40,8 @@ Chip8::~Chip8 () {
 }
 
 void Chip8::load_rom(const char* rompath) {
+    // load_rom reads a rom file from disk, and dumps the binary data in memory starting at 0x200
+
     std::ifstream rom(rompath, std::ios::binary); 
     int offset = 0;
     while(rom) {
@@ -44,10 +52,12 @@ void Chip8::load_rom(const char* rompath) {
         }
         ++offset;
     }
-    std::cout << std::endl;
 }
 
 void Chip8::fetch_instruction() {
+    // fetch_instruction is responsible for fetching the current opcode from memory and increasing the
+    // program counter to the next instruction.
+
     // Each opcode is 2 bytes long, so we fetch the byte in memory at address m_program_counter, shift
     // if left by 8 bits and take the bitwise or with the value at address m_program_counter + 1.
     m_opcode = (m_memory[m_program_counter] << 8) | m_memory[m_program_counter + 1];
@@ -55,17 +65,21 @@ void Chip8::fetch_instruction() {
 }
 
 void Chip8::run_instruction() {
-    uint8_t first_nibble =  (m_opcode & 0xf000) >> 12;
+    // run_instruction is responsible for running the instruction fetched by fetch_instruction.
+    // It accomplishes this by deconstructin the instruction into 4 nibbles (1 nibble is 4 bits),
+    // does a swich on the first nibble and then act accordingly.
+    
+    uint8_t first_nibble  = (m_opcode & 0xf000) >> 12;
     uint8_t second_nibble = (m_opcode & 0x0f00) >> 8;
-    uint8_t third_nibble = (m_opcode & 0x00f0) >> 4;
+    uint8_t third_nibble  = (m_opcode & 0x00f0) >> 4;
     uint8_t fourth_nibble = (m_opcode & 0x000f) >> 0;
     
-    uint8_t register_x   = second_nibble;
-    uint8_t register_y   = third_nibble;
+    uint8_t register_x    = second_nibble;
+    uint8_t register_y    = third_nibble;
 
-    uint8_t value_n      = (m_opcode & 0x000f);
-    uint8_t value_nn     = (m_opcode & 0x00ff);
-    uint16_t value_nnn   = (m_opcode & 0x0fff);
+    uint8_t value_n       = (m_opcode & 0x000f);
+    uint8_t value_nn      = (m_opcode & 0x00ff);
+    uint16_t value_nnn    = (m_opcode & 0x0fff);
 
     switch(first_nibble) { 
         case 0x2:
@@ -88,8 +102,10 @@ void Chip8::run_instruction() {
 }
 
 void Chip8::dump_memory() {
+    // dump_memory dumps all 4096 bytes of memory to stdout
+
     for (int i = 0; i < MEMORY_SIZE; ++i) {
         std::cout << m_memory[i];
     }
-    std::cout << std::hex << std::endl;
+    std::cout << std::endl;
 }
